a3a0087586b036439faeb9e965b95a810b309bcb
avenj <avenj@eris.oppresses.us>
2015-06-06 04:32:56 +0000

    reasonably complete User

--- lib/IRC/Server/State/User.pm
+++ lib/IRC/Server/State/User.pm
@@ -13,6 +13,48 @@ has casemap => (
 );
 
 
+has $_ => (
+  required  => 1,
+  is        => 'ro',
+  isa       => Str,
+  writer    => "set_$_",
+) for qw/
+  nickname
+  username
+  realname
+  hostname
+/;
+
+has realhost => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Str,
+  writer    => 'set_realhost',
+  predicate => 1,
+  builder   => sub { shift->hostname },
+);
+
+has ipaddr => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Str,
+  predicate => 1,
+  builder   => sub { '255.255.255.255' },
+);
+
+has modes => (
+  # FIXME
+);
+
+has meta => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => HashObj,
+  coerce    => 1,
+  predicate => 1,
+  builder   => sub { +{} },
+);
+
 with 'IRC::Server::State::Role::ChannelCollection';
 
 1;
