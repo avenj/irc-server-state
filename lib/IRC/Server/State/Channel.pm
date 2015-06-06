13a857083833e71322729989d71abe7bdd5caa67
avenj <avenj@eris.oppresses.us>
2015-06-06 02:51:24 +0000

    topic handling

--- lib/IRC/Server/State/Channel.pm
+++ lib/IRC/Server/State/Channel.pm
@@ -1,5 +1,7 @@
 package IRC::Server::State::Channel;
 
+use Scalar::Util 'reftype';
+
 use Types::Standard      -all;
 use List::Objects::Types -all;
 use IRC::Server::State::Types -all;
@@ -17,6 +19,50 @@ has casemap => (
   is        => 'ro',
   isa       => CasemapRef,
 );
+
+has name => (
+  required  => 1,
+  is        => 'ro',
+  isa       => Str,
+);
+
+has lists => (
+  # FIXME
+);
+
+has modes => (
+  # FIXME
+);
+
+
+has topic => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => InflatedHash[qw/string ts setter/],
+  coerce    => 1,
+  builder   => sub {
+    +{ string => '', ts => time, setter => '' }
+  },
+);
+
+sub set_topic {
+  my ($self, %params) = @_;
+  my $topic = $self->topic;
+  for (keys %params) {
+    confess "Unknown set_topic option '$_'"
+      unless $_ eq 'string' or $_ eq 'ts' or $_ eq 'setter';
+    $topic->{$_} = $params{$_}
+  }
+  $topic
+}
+
+has ts => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => StrictNum,
+  builder   => sub { time },
+);
+
 
 with 'IRC::Server::State::Role::UserCollection';
 
