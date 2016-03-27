969de40287c6965f00b5b002098d37e9f300135a
Jon Portnoy <avenj@cobaltirc.org>
2016-03-27 15:18:40 -0400

    use ImmutableArrays for ModeConfig mode sets

--- lib/IRC/Server/State/ModeConfig.pm
+++ lib/IRC/Server/State/ModeConfig.pm
@@ -3,6 +3,7 @@ package IRC::Server::State::ModeConfig;
 use strictures 2;
 use Carp;
 
+use List::Objects::Types -types;
 use Types::Standard -types;
 
 use IRC::Toolkit::Modes ();
@@ -13,20 +14,22 @@ use Moo;
 has param_always => (
   required  => 1,
   is        => 'ro',
-  isa       => ArrayRef,
+  isa       => ImmutableArray,
+  coerce    => 1,
 );
 
 has param_when_set => (
   required  => 1,
   is        => 'ro',
-  isa       => ArrayRef,
+  isa       => ImmutableArray,
+  coerce    => 1,
 );
 
 sub mode_to_array {
   my ($self, $mode_string, @params) = @_;
   IRC::Toolkit::Modes::mode_to_array( $mode_string,
-    param_always => $self->param_always,
-    param_set    => $self->param_when_set,
+    param_always => $self->param_always->unbless,
+    param_set    => $self->param_when_set->unbless,
     ( @params ? (params => [@params]) : () )
   )
 }
